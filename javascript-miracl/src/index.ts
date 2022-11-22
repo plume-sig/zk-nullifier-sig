c082b8ed6fc4611fb69f53a38d9aee120083a956
Richard Liu <richard.y.liu@berkeley.edu>
2022-11-22 02:55:04 -0800

    hashMPk

--- javascript-miracl/src/index.ts
+++ javascript-miracl/src/index.ts
@@ -1,35 +1,41 @@
+import {
+  privateKeyTweakMul as multiplyOnCurve,
+  publicKeyCreate,
+} from "secp256k1";
+import { hexToUint8Array, messageToUint8Array } from "./utils/encoding";
 import hashToCurve from "./utils/hashToCurve";
-import secp256k1 from "secp256k1";
 
-export function computeH(publicKey, message) {
-  // TODO
+export function computeHashMPk(message: Uint8Array, publicKey: Uint8Array) {
+  // Concatenate message and publicKey
+  const input = new Uint8Array(message.length + publicKey.length);
+  input.set(message);
+  input.set(publicKey, message.length);
+  return hashToCurve(Array.from(input));
+}
+
+export function computeNullifier(h, secretKey) {
+  // return multiplyOnCurve(h, secretKey);
 }
 
 export function computeC(g, pk, h, nul, g_r, z) {
   // TODO
 }
 
-const testSecretKey =
-  "519b423d715f8b581f4fa8ee59f4771a5b44c8130b4e3eacca54a56dda72b464";
+const testSecretKey = hexToUint8Array(
+  "519b423d715f8b581f4fa8ee59f4771a5b44c8130b4e3eacca54a56dda72b464"
+);
 
-const testPublicKey = secp256k1.publicKeyCreate(testSecretKey);
-console.log({ testPublicKey });
+const testPublicKey = publicKeyCreate(testSecretKey);
 
-const testR =
-  "93b9323b629f251b8f3fc2dd11f4672c5544e8230d493eceea98a90bda789808";
+const testR = hexToUint8Array(
+  "93b9323b629f251b8f3fc2dd11f4672c5544e8230d493eceea98a90bda789808"
+);
 
-const testM = "An example app message string";
+const testMessage = messageToUint8Array("An example app message string");
 
-const testBytesToHash = [
-  65, 110, 32, 101, 120, 97, 109, 112, 108, 101, 32, 97, 112, 112, 32, 109, 101,
-  115, 115, 97, 103, 101, 32, 115, 116, 114, 105, 110, 103, 3, 12, 236, 2, 142,
-  224, 141, 9, 224, 38, 114, 166, 131, 16, 129, 67, 84, 249, 234, 191, 255, 13,
-  230, 218, 204, 28, 211, 167, 116, 73, 96, 118, 174,
-];
-
-const h = hashToCurve(testBytesToHash);
-console.log("h.x:", h.x.toString());
-console.log("h.y:", h.y.toString());
+const hashMPk = computeHashMPk(testMessage, Buffer.from(testPublicKey));
+console.log(`h.x: ${hashMPk.x.toString()}`);
+console.log(`h.y: ${hashMPk.y.toString()}`);
 
 // Expected
 // h.x: bcac2d0e12679f23c218889395abcdc01f2affbc49c54d1136a2190db0800b65
--- javascript-miracl/src/utils/encoding.ts
+++ javascript-miracl/src/utils/encoding.ts
@@ -1,3 +1,13 @@
+const utf8Encoder = new TextEncoder();
+
+export function messageToUint8Array(message: string): Uint8Array {
+  return utf8Encoder.encode(message);
+}
+
+export function hexToUint8Array(hexString: string): Uint8Array {
+  return Uint8Array.from(Buffer.from(hexString, "hex"));
+}
+
 export function asciitobytes(s: string): number[] {
   var b = [],
     i: number;
