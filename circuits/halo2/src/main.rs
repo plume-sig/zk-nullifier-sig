a6945893d2cc715ebaab1449c72d97ebc601ec37
blakemscurr <blakemscurr@gmail.com>
2023-05-15 12:07:31 +1200

    Complete a/b^c function.

--- circuits/halo2/src/main.rs
+++ circuits/halo2/src/main.rs
@@ -1,3 +1,4 @@
+use crate::fields::{fp::FpConfig, FieldChip};
 use halo2_base::{
     gates::{GateInstructions, RangeInstructions},
     utils::{modulus, CurveAffineExt, PrimeField},
@@ -12,6 +13,7 @@ use halo2_ecc::ecc::{ec_add_unequal, scalar_multiply, EcPoint};
 
 // Computes a/b^c where a and b are EC points, and c is a scalar
 // Both of the main equations in PLUME are of this form
+// Equivalent to https://github.com/plume-sig/zk-nullifier-sig/blob/3288b7b9115e86e63a5a5df616d0affc89811f9e/circuits/verify_nullifier.circom#L265
 //
 // TODO: what is v? I'm just cargo culting it rn
 // F is the prime field of the circuit we're working in. I.e., the field of the proof system
@@ -23,8 +25,11 @@ pub fn a_div_b_pow_c<'v, F: PrimeField, CF: PrimeField, SF: PrimeField, GA>(
     base_chip: &FpConfig<F, CF>,
     ctx: &mut Context<'v, F>,
     var_window_bits: usize, // TODO: what is this?
+    a: &EcPoint<F, <FpConfig<F, CF> as FieldChip<F>>::FieldPoint<'v>>,
+    b: &EcPoint<F, <FpConfig<F, CF> as FieldChip<F>>::FieldPoint<'v>>,
     c: &CRTInteger<'v, F>,
-) where
+) -> EcPoint<F, <FpConfig<F, CF> as FieldChip<F>>::FieldPoint<'v>>
+where
     GA: CurveAffineExt<Base = CF, ScalarExt = SF>,
 {
     let b_pow_c = scalar_multiply::<F, _>(
@@ -35,6 +40,19 @@ pub fn a_div_b_pow_c<'v, F: PrimeField, CF: PrimeField, SF: PrimeField, GA>(
         base_chip.limb_bits,
         var_window_bits,
     );
+
+    // Calculates inverse of b^c by finding the modular inverse of its y coordinate
+    let scalar_chip = FpConfig::<F, SF>::construct(
+        base_chip.range.clone(),
+        base_chip.limb_bits,
+        base_chip.num_limbs,
+        modulus::<SF>(),
+    );
+
+    let b_pow_c_inv = EcPoint::construct(b_pow_c.x.clone(), base_chip.negate(ctx, &b_pow_c.y)); // TODO: use ECC chip's negate method - I just found it easier to copy the code short term
+
+    // Calculates a * (b^c)-1
+    ec_add_unequal(base_chip, ctx, a, &b_pow_c_inv, false)
 }
 
 fn main() {
