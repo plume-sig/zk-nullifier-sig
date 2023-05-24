e5ac92e5957b91f071e704374c8d2063877e1c9c
blakemscurr <blakemscurr@gmail.com>
2023-05-24 15:37:50 +1200

    Comment out non-compiling tests.

--- circuits/halo2/src/main.rs
+++ circuits/halo2/src/main.rs
@@ -10,6 +10,19 @@ use halo2_ecc::fields::{fp::FpConfig, FieldChip};
 use halo2_ecc::ecc::fixed_base;
 use halo2_ecc::ecc::{ec_add_unequal, scalar_multiply, EcPoint};
 
+pub fn verify_nullifier<'v, F: PrimeField, CF: PrimeField, SF: PrimeField, GA>(
+    base_chip: &FpConfig<F, CF>,
+    ctx: &mut Context<'v, F>,
+    var_window_bits: usize,
+    a: &EcPoint<F, <FpConfig<F, CF> as FieldChip<F>>::FieldPoint<'v>>,
+    b: &EcPoint<F, <FpConfig<F, CF> as FieldChip<F>>::FieldPoint<'v>>,
+    c: &CRTInteger<'v, F>,
+) -> EcPoint<F, <FpConfig<F, CF> as FieldChip<F>>::FieldPoint<'v>>
+where
+    GA: CurveAffineExt<Base = CF, ScalarExt = SF>,
+{
+}
+
 // Computes a/b^c where a and b are EC points, and c is a scalar
 // Both of the main equations in PLUME are of this form
 // Equivalent to https://github.com/plume-sig/zk-nullifier-sig/blob/3288b7b9115e86e63a5a5df616d0affc89811f9e/circuits/verify_nullifier.circom#L265
@@ -58,226 +71,226 @@ fn main() {
     println!("Hello, world!");
 }
 
-mod test {
-    #![allow(non_snake_case)]
-    use halo2_base::{utils::PrimeField, SKIP_FIRST_PASS};
-    // use serde::{Deserialize, Serialize};
-    use std::fs::File;
-    use std::marker::PhantomData;
-    use std::{env::var, io::Write};
+// mod test {
+//     #![allow(non_snake_case)]
+//     use halo2_base::{utils::PrimeField, SKIP_FIRST_PASS};
+//     // use serde::{Deserialize, Serialize};
+//     use std::fs::File;
+//     use std::marker::PhantomData;
+//     use std::{env::var, io::Write};
 
-    use halo2_base::halo2_proofs::{
-        arithmetic::CurveAffine,
-        circuit::*,
-        dev::MockProver,
-        halo2curves::bn256::{Bn256, Fr, G1Affine},
-        halo2curves::secp256k1::{Fp, Fq, Secp256k1Affine},
-        plonk::*,
-        poly::commitment::ParamsProver,
-        transcript::{Blake2bRead, Blake2bWrite, Challenge255},
-    };
+//     use halo2_base::halo2_proofs::{
+//         arithmetic::CurveAffine,
+//         circuit::*,
+//         dev::MockProver,
+//         halo2curves::bn256::{Bn256, Fr, G1Affine},
+//         halo2curves::secp256k1::{Fp, Fq, Secp256k1Affine},
+//         plonk::*,
+//         poly::commitment::ParamsProver,
+//         transcript::{Blake2bRead, Blake2bWrite, Challenge255},
+//     };
 
-    use halo2_base::utils::{biguint_to_fe, fe_to_biguint, modulus};
-    use halo2_ecc::fields::fp::FpConfig;
-    use halo2_ecc::{
-        ecc::{ecdsa::ecdsa_verify_no_pubkey_check, EccChip},
-        fields::{fp::FpStrategy, FieldChip},
-    };
+//     use halo2_base::utils::{biguint_to_fe, fe_to_biguint, modulus};
+//     use halo2_ecc::fields::fp::FpConfig;
+//     use halo2_ecc::{
+//         ecc::{ecdsa::ecdsa_verify_no_pubkey_check, EccChip},
+//         fields::{fp::FpStrategy, FieldChip},
+//     };
 
-    type FpChip<F> = FpConfig<F, Fp>;
+//     type FpChip<F> = FpConfig<F, Fp>;
 
-    struct CircuitParams {
-        strategy: FpStrategy,
-        degree: u32,
-        num_advice: usize,
-        num_lookup_advice: usize,
-        num_fixed: usize,
-        lookup_bits: usize,
-        limb_bits: usize,
-        num_limbs: usize,
-    }
+//     struct CircuitParams {
+//         strategy: FpStrategy,
+//         degree: u32,
+//         num_advice: usize,
+//         num_lookup_advice: usize,
+//         num_fixed: usize,
+//         lookup_bits: usize,
+//         limb_bits: usize,
+//         num_limbs: usize,
+//     }
 
-    pub struct ECDSACircuit<F> {
-        pub r: Option<Fq>,
-        pub s: Option<Fq>,
-        pub msghash: Option<Fq>,
-        pub pk: Option<Secp256k1Affine>,
-        pub G: Secp256k1Affine,
-        pub _marker: PhantomData<F>,
-    }
-    impl<F: PrimeField> Default for ECDSACircuit<F> {
-        fn default() -> Self {
-            Self {
-                r: None,
-                s: None,
-                msghash: None,
-                pk: None,
-                G: Secp256k1Affine::generator(),
-                _marker: PhantomData,
-            }
-        }
-    }
+//     pub struct ECDSACircuit<F> {
+//         pub r: Option<Fq>,
+//         pub s: Option<Fq>,
+//         pub msghash: Option<Fq>,
+//         pub pk: Option<Secp256k1Affine>,
+//         pub G: Secp256k1Affine,
+//         pub _marker: PhantomData<F>,
+//     }
+//     impl<F: PrimeField> Default for ECDSACircuit<F> {
+//         fn default() -> Self {
+//             Self {
+//                 r: None,
+//                 s: None,
+//                 msghash: None,
+//                 pk: None,
+//                 G: Secp256k1Affine::generator(),
+//                 _marker: PhantomData,
+//             }
+//         }
+//     }
 
-    impl<F: PrimeField> Circuit<F> for ECDSACircuit<F> {
-        type Config = FpChip<F>;
-        type FloorPlanner = SimpleFloorPlanner;
+//     impl<F: PrimeField> Circuit<F> for ECDSACircuit<F> {
+//         type Config = FpChip<F>;
+//         type FloorPlanner = SimpleFloorPlanner;
 
-        fn without_witnesses(&self) -> Self {
-            Self::default()
-        }
+//         fn without_witnesses(&self) -> Self {
+//             Self::default()
+//         }
 
-        fn configure(meta: &mut ConstraintSystem<F>) -> Self::Config {
-            let path = var("ECDSA_CONFIG")
-                .unwrap_or_else(|_| "./src/secp256k1/configs/ecdsa_circuit.config".to_string());
-            let params: CircuitParams = serde_json::from_reader(
-                File::open(&path).unwrap_or_else(|_| panic!("{path:?} file should exist")),
-            )
-            .unwrap();
+//         fn configure(meta: &mut ConstraintSystem<F>) -> Self::Config {
+//             let path = var("ECDSA_CONFIG")
+//                 .unwrap_or_else(|_| "./src/secp256k1/configs/ecdsa_circuit.config".to_string());
+//             let params: CircuitParams = serde_json::from_reader(
+//                 File::open(&path).unwrap_or_else(|_| panic!("{path:?} file should exist")),
+//             )
+//             .unwrap();
 
-            FpChip::<F>::configure(
-                meta,
-                params.strategy,
-                &[params.num_advice],
-                &[params.num_lookup_advice],
-                params.num_fixed,
-                params.lookup_bits,
-                params.limb_bits,
-                params.num_limbs,
-                modulus::<Fp>(),
-                0,
-                params.degree as usize,
-            )
-        }
+//             FpChip::<F>::configure(
+//                 meta,
+//                 params.strategy,
+//                 &[params.num_advice],
+//                 &[params.num_lookup_advice],
+//                 params.num_fixed,
+//                 params.lookup_bits,
+//                 params.limb_bits,
+//                 params.num_limbs,
+//                 modulus::<Fp>(),
+//                 0,
+//                 params.degree as usize,
+//             )
+//         }
 
-        fn synthesize(
-            &self,
-            fp_chip: Self::Config,
-            mut layouter: impl Layouter<F>,
-        ) -> Result<(), Error> {
-            fp_chip.range.load_lookup_table(&mut layouter)?;
+//         fn synthesize(
+//             &self,
+//             fp_chip: Self::Config,
+//             mut layouter: impl Layouter<F>,
+//         ) -> Result<(), Error> {
+//             fp_chip.range.load_lookup_table(&mut layouter)?;
 
-            let limb_bits = fp_chip.limb_bits;
-            let num_limbs = fp_chip.num_limbs;
-            let _num_fixed = fp_chip.range.gate.constants.len();
-            let _lookup_bits = fp_chip.range.lookup_bits;
-            let _num_advice = fp_chip.range.gate.num_advice;
+//             let limb_bits = fp_chip.limb_bits;
+//             let num_limbs = fp_chip.num_limbs;
+//             let _num_fixed = fp_chip.range.gate.constants.len();
+//             let _lookup_bits = fp_chip.range.lookup_bits;
+//             let _num_advice = fp_chip.range.gate.num_advice;
 
-            let mut first_pass = SKIP_FIRST_PASS;
-            // ECDSA verify
-            layouter.assign_region(
-                || "ECDSA",
-                |region| {
-                    if first_pass {
-                        first_pass = false;
-                        return Ok(());
-                    }
+//             let mut first_pass = SKIP_FIRST_PASS;
+//             // ECDSA verify
+//             layouter.assign_region(
+//                 || "ECDSA",
+//                 |region| {
+//                     if first_pass {
+//                         first_pass = false;
+//                         return Ok(());
+//                     }
 
-                    let mut aux = fp_chip.new_context(region);
-                    let ctx = &mut aux;
+//                     let mut aux = fp_chip.new_context(region);
+//                     let ctx = &mut aux;
 
-                    let (r_assigned, s_assigned, m_assigned) = {
-                        let fq_chip = FpConfig::<F, Fq>::construct(
-                            fp_chip.range.clone(),
-                            limb_bits,
-                            num_limbs,
-                            modulus::<Fq>(),
-                        );
+//                     let (r_assigned, s_assigned, m_assigned) = {
+//                         let fq_chip = FpConfig::<F, Fq>::construct(
+//                             fp_chip.range.clone(),
+//                             limb_bits,
+//                             num_limbs,
+//                             modulus::<Fq>(),
+//                         );
 
-                        let m_assigned = fq_chip.load_private(
-                            ctx,
-                            FpConfig::<F, Fq>::fe_to_witness(
-                                &self.msghash.map_or(Value::unknown(), Value::known),
-                            ),
-                        );
+//                         let m_assigned = fq_chip.load_private(
+//                             ctx,
+//                             FpConfig::<F, Fq>::fe_to_witness(
+//                                 &self.msghash.map_or(Value::unknown(), Value::known),
+//                             ),
+//                         );
 
-                        let r_assigned = fq_chip.load_private(
-                            ctx,
-                            FpConfig::<F, Fq>::fe_to_witness(
-                                &self.r.map_or(Value::unknown(), Value::known),
-                            ),
-                        );
-                        let s_assigned = fq_chip.load_private(
-                            ctx,
-                            FpConfig::<F, Fq>::fe_to_witness(
-                                &self.s.map_or(Value::unknown(), Value::known),
-                            ),
-                        );
-                        (r_assigned, s_assigned, m_assigned)
-                    };
+//                         let r_assigned = fq_chip.load_private(
+//                             ctx,
+//                             FpConfig::<F, Fq>::fe_to_witness(
+//                                 &self.r.map_or(Value::unknown(), Value::known),
+//                             ),
+//                         );
+//                         let s_assigned = fq_chip.load_private(
+//                             ctx,
+//                             FpConfig::<F, Fq>::fe_to_witness(
+//                                 &self.s.map_or(Value::unknown(), Value::known),
+//                             ),
+//                         );
+//                         (r_assigned, s_assigned, m_assigned)
+//                     };
 
-                    let ecc_chip = EccChip::<F, FpChip<F>>::construct(fp_chip.clone());
-                    let pk_assigned = ecc_chip.load_private(
-                        ctx,
-                        (
-                            self.pk.map_or(Value::unknown(), |pt| Value::known(pt.x)),
-                            self.pk.map_or(Value::unknown(), |pt| Value::known(pt.y)),
-                        ),
-                    );
-                    // test ECDSA
-                    let ecdsa = ecdsa_verify_no_pubkey_check::<F, Fp, Fq, Secp256k1Affine>(
-                        &ecc_chip.field_chip,
-                        ctx,
-                        &pk_assigned,
-                        &r_assigned,
-                        &s_assigned,
-                        &m_assigned,
-                        4,
-                        4,
-                    );
+//                     let ecc_chip = EccChip::<F, FpChip<F>>::construct(fp_chip.clone());
+//                     let pk_assigned = ecc_chip.load_private(
+//                         ctx,
+//                         (
+//                             self.pk.map_or(Value::unknown(), |pt| Value::known(pt.x)),
+//                             self.pk.map_or(Value::unknown(), |pt| Value::known(pt.y)),
+//                         ),
+//                     );
+//                     // test ECDSA
+//                     let ecdsa = ecdsa_verify_no_pubkey_check::<F, Fp, Fq, Secp256k1Affine>(
+//                         &ecc_chip.field_chip,
+//                         ctx,
+//                         &pk_assigned,
+//                         &r_assigned,
+//                         &s_assigned,
+//                         &m_assigned,
+//                         4,
+//                         4,
+//                     );
 
-                    // IMPORTANT: this copies cells to the lookup advice column to perform range check lookups
-                    // This is not optional.
-                    fp_chip.finalize(ctx);
+//                     // IMPORTANT: this copies cells to the lookup advice column to perform range check lookups
+//                     // This is not optional.
+//                     fp_chip.finalize(ctx);
 
-                    #[cfg(feature = "display")]
-                    if self.r.is_some() {
-                        println!("ECDSA res {ecdsa:?}");
+//                     #[cfg(feature = "display")]
+//                     if self.r.is_some() {
+//                         println!("ECDSA res {ecdsa:?}");
 
-                        ctx.print_stats(&["Range"]);
-                    }
-                    Ok(())
-                },
-            )
-        }
-    }
+//                         ctx.print_stats(&["Range"]);
+//                     }
+//                     Ok(())
+//                 },
+//             )
+//         }
+//     }
 
-    #[cfg(test)]
-    #[test]
-    fn test_secp256k1_ecdsa() {
-        let mut folder = std::path::PathBuf::new();
-        folder.push("./src/secp256k1");
-        folder.push("configs/ecdsa_circuit.config");
-        let params_str = std::fs::read_to_string(folder.as_path())
-            .expect("src/secp256k1/configs/ecdsa_circuit.config file should exist");
-        let params: CircuitParams = serde_json::from_str(params_str.as_str()).unwrap();
-        let K = params.degree;
+//     #[cfg(test)]
+//     #[test]
+//     fn test_secp256k1_ecdsa() {
+//         let mut folder = std::path::PathBuf::new();
+//         folder.push("./src/secp256k1");
+//         folder.push("configs/ecdsa_circuit.config");
+//         let params_str = std::fs::read_to_string(folder.as_path())
+//             .expect("src/secp256k1/configs/ecdsa_circuit.config file should exist");
+//         let params: CircuitParams = serde_json::from_str(params_str.as_str()).unwrap();
+//         let K = params.degree;
 
-        // generate random pub key and sign random message
-        let G = Secp256k1Affine::generator();
-        let sk = <Secp256k1Affine as CurveAffine>::ScalarExt::random(OsRng);
-        let pubkey = Secp256k1Affine::from(G * sk);
-        let msg_hash = <Secp256k1Affine as CurveAffine>::ScalarExt::random(OsRng);
+//         // generate random pub key and sign random message
+//         let G = Secp256k1Affine::generator();
+//         let sk = <Secp256k1Affine as CurveAffine>::ScalarExt::random(OsRng);
+//         let pubkey = Secp256k1Affine::from(G * sk);
+//         let msg_hash = <Secp256k1Affine as CurveAffine>::ScalarExt::random(OsRng);
 
-        let k = <Secp256k1Affine as CurveAffine>::ScalarExt::random(OsRng);
-        let k_inv = k.invert().unwrap();
+//         let k = <Secp256k1Affine as CurveAffine>::ScalarExt::random(OsRng);
+//         let k_inv = k.invert().unwrap();
 
-        let r_point = Secp256k1Affine::from(G * k).coordinates().unwrap();
-        let x = r_point.x();
-        let x_bigint = fe_to_biguint(x);
-        let r = biguint_to_fe::<Fq>(&(x_bigint % modulus::<Fq>()));
-        let s = k_inv * (msg_hash + (r * sk));
+//         let r_point = Secp256k1Affine::from(G * k).coordinates().unwrap();
+//         let x = r_point.x();
+//         let x_bigint = fe_to_biguint(x);
+//         let r = biguint_to_fe::<Fq>(&(x_bigint % modulus::<Fq>()));
+//         let s = k_inv * (msg_hash + (r * sk));
 
-        let circuit = ECDSACircuit::<Fr> {
-            r: Some(r),
-            s: Some(s),
-            msghash: Some(msg_hash),
-            pk: Some(pubkey),
-            G,
-            _marker: PhantomData,
-        };
+//         let circuit = ECDSACircuit::<Fr> {
+//             r: Some(r),
+//             s: Some(s),
+//             msghash: Some(msg_hash),
+//             pk: Some(pubkey),
+//             G,
+//             _marker: PhantomData,
+//         };
 
-        let prover = MockProver::run(K, &circuit, vec![]).unwrap();
-        //prover.assert_satisfied();
-        assert_eq!(prover.verify(), Ok(()));
-    }
-}
+//         let prover = MockProver::run(K, &circuit, vec![]).unwrap();
+//         //prover.assert_satisfied();
+//         assert_eq!(prover.verify(), Ok(()));
+//     }
+// }
