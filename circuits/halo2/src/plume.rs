f77e371765eb3518b7e065342e219fc9ec378ba0
shryasss <shreyas_londhe@outlook.com>
2024-01-11 17:47:17 +0530

    fix: refactoring

--- circuits/halo2/src/plume.rs
+++ circuits/halo2/src/plume.rs
@@ -1,11 +1,12 @@
 use halo2_base::{
-    gates::RangeChip, halo2_proofs::halo2curves::secp256k1::Secp256k1Affine, utils::BigPrimeField,
-    AssignedValue, Context,
+    gates::RangeChip,
+    utils::{BigPrimeField, CurveAffineExt},
+    Context,
 };
 use halo2_ecc::{
     bigint::ProperCrtUint,
-    ecc::{EcPoint, EccChip},
-    secp256k1::FpChip,
+    ecc::{fixed_base, scalar_multiply, EcPoint, EccChip},
+    fields::fp::FpChip,
 };
 
 #[derive(Clone, Debug)]
@@ -19,16 +20,19 @@ struct PlumeInput<F: BigPrimeField> {
     m: ProperCrtUint<F>,
 }
 
-fn verify_plume<F: BigPrimeField>(
+fn verify_plume<F: BigPrimeField, CF: BigPrimeField, SF: BigPrimeField, GA>(
     ctx: &mut Context<F>,
     range: &RangeChip<F>,
+    chip: &EccChip<F, FpChip<F, CF>>,
     limb_bits: usize,
     num_limbs: usize,
     max_bits: usize,
-    window_bits: usize,
+    fixed_window_bits: usize,
+    var_window_bits: usize,
     input: PlumeInput<F>,
-    make_public: &mut Vec<AssignedValue<F>>,
-) {
+) where
+    GA: CurveAffineExt<Base = CF, ScalarExt = SF>,
+{
     let PlumeInput {
         nullifier,
         s,
@@ -37,43 +41,57 @@ fn verify_plume<F: BigPrimeField>(
         m,
     } = input;
 
-    let fp_chip = FpChip::<F>::new(range, limb_bits, num_limbs);
-    let ecc_chip = EccChip::<F, FpChip<F>>::new(&fp_chip);
+    let base_chip = chip.field_chip;
+    let scalar_chip =
+        FpChip::<F, SF>::new(base_chip.range, base_chip.limb_bits, base_chip.num_limbs);
 
-    // 1. compute hash[m, pk]
+    // 1. compute hash[m, pk]test_plume_verify
     // TODO
 
     // 2. compute g^s
-    let gs = ecc_chip.fixed_base_scalar_mult(
+    let gs = fixed_base::scalar_multiply(
+        base_chip,
         ctx,
-        &Secp256k1Affine::generator(),
+        &GA::generator(),
         s.limbs().to_vec(),
-        max_bits,
-        window_bits,
+        base_chip.limb_bits,
+        fixed_window_bits,
     );
+    println!("reached 4");
 
     // 3. compute pk^c
-    let pkc =
-        ecc_chip.scalar_mult::<Secp256k1Affine>(ctx, pk, c.limbs().to_vec(), max_bits, window_bits);
+    let pkc = scalar_multiply::<_, _, GA>(
+        base_chip,
+        ctx,
+        pk,
+        c.limbs().to_vec(),
+        base_chip.limb_bits,
+        var_window_bits,
+    );
+    println!("reached 5");
 
     // 4. compute g^s / pk^c
-    let pkc_inv = ecc_chip.negate(ctx, pkc);
-    let gs_pkc = ecc_chip.add_unequal(ctx, &gs, &pkc_inv, false);
+    let pkc_inv = chip.negate(ctx, pkc);
+    // let gs_pkc = ecc_chip.add_unequal(ctx, &gs, &pkc_inv, false);
+    println!("reached 6");
 
     // 5. compute hash[m, pk]^s
     // TODO
 
     // 6. compute nullifier^c
-    let nullifierc = ecc_chip.scalar_mult::<Secp256k1Affine>(
+    let nullifierc = scalar_multiply::<_, _, GA>(
+        base_chip,
         ctx,
         nullifier.clone(),
         c.limbs().to_vec(),
-        max_bits,
-        window_bits,
+        base_chip.limb_bits,
+        var_window_bits,
     );
+    println!("reached 7");
 
     // 7. compute hash[m, pk]^s / (nullifier)^c
-    let nullifierc_inv = ecc_chip.negate(ctx, nullifierc);
+    let nullifierc_inv = chip.negate(ctx, nullifierc);
+    println!("reached 8")
     // TODO
 
     // 8. compute hash2(g, pk, hash[m, pk], nullifier, g^s / pk^c, hash[m, pk]^s / nullifier^c)
