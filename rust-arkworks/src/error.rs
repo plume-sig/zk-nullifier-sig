80bdc99f361ee090a6644dd1089e7e7bd49eb02d
skaunov <skaunov@disroot.org>
2024-02-10 14:29:52 +0300

    current progress

--- rust-arkworks/src/error.rs
+++ rust-arkworks/src/error.rs
@@ -3,7 +3,7 @@
 
 // use thiserror::Error;
 
-/// This is an error that could occur when running a cryptograhic primitive
+// /// This is an error that could occur when running a cryptograhic primitive
 // #[derive(Error, Debug, PartialEq)]
 // pub enum CryptoError {
 // #[error("Cannot hash to curve")]
@@ -14,13 +14,18 @@
 // }
 
 // Let's outline what errors will be in `~0.4.0`
+/// It's an interim `enum` between legacy definition of the errors and prospective which will be relying on [`ark_ec::hashing::HashToCurveError`]. 
 #[derive(Debug, Clone)]
 pub enum HashToCurveError {
+    /// Mimics the `ark_ec::hashing::HashToCurveError` enum
     UnsupportedCurveError(String),
+    /// Mimics the `ark_ec::hashing::HashToCurveError` enum
     MapToCurveError(String),
     /* let's add two more items to absorb everything
     in `crate::hash_to_curve` which is
     subject to deprecation */
+    /// Absorbs any legacy error in [`mod@crate::hash_to_curve`]. They will be deprecated with upgrade to `~0.4.0`.
     Legacy,
+    /// A special case for a reference function. It will be moved to <./examples> with the upgrade to `~0.4.0`.
     ReferenceTryAndIncrement,
 }
---
+++ rust-k256/src/utils.rs
@@ -0,0 +1,25 @@
+use super::*;
+use k256::{
+    elliptic_curve::{
+        hash2curve::{ExpandMsgXmd, GroupDigest},
+        sec1::ToEncodedPoint,
+    },
+    ProjectivePoint, Secp256k1,
+}; // requires 'getrandom' feature
+
+// Hashes two values to the curve
+pub(crate) fn hash_to_curve(
+    m: &[u8],
+    pk: &ProjectivePoint,
+) -> Result<ProjectivePoint, k256::elliptic_curve::Error> {
+    Secp256k1::hash_from_bytes::<ExpandMsgXmd<Sha256>>(
+        &[[m, &encode_pt(pk)].concat().as_slice()],
+        //b"CURVE_XMD:SHA-256_SSWU_RO_",
+        &[DST],
+    )
+}
+
+/// Encodes the point by compressing it to 33 bytes
+pub(crate) fn encode_pt(point: &ProjectivePoint) -> Vec<u8> {
+    point.to_encoded_point(true).to_bytes().to_vec()
+}
