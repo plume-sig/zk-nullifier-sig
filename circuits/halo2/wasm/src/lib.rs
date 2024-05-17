56e9c80a572b6301fb2ccfa6f68df8a52dcaf6dd
shreyas-londhe <shreyas_londhe@outlook.com>
2024-05-17 06:52:43 +0530

    feat: plume-wasm npm package

---
+++ circuits/halo2/wasm/src/lib.rs
@@ -0,0 +1,99 @@
+mod utils;
+
+use halo2_wasm::{
+  halo2_base::{
+    gates::{ circuit::builder::BaseCircuitBuilder, RangeChip, RangeInstructions },
+    poseidon::hasher::{ spec::OptimizedPoseidonSpec, PoseidonHasher },
+  },
+  halo2_ecc::secp256k1::{ FpChip, FqChip },
+  halo2lib::ecc::{ Bn254Fr as Fr, EccChip, FieldChip },
+  Halo2Wasm,
+};
+use plume_halo2::plume::{ verify_plume, PlumeInput };
+use serde::{ Deserialize, Serialize };
+use tsify::Tsify;
+use std::{ cell::RefCell, rc::Rc };
+use wasm_bindgen::prelude::*;
+use utils::{ parse_compressed_point, parse_scalar };
+
+#[derive(Tsify, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+#[tsify(into_wasm_abi, from_wasm_abi)]
+pub struct PlumeVerifyInput {
+  // Public
+  pub nullifier: String,
+  pub s: String,
+
+  // Private
+  pub c: String,
+  pub message: String,
+  pub public_key: String,
+}
+
+#[wasm_bindgen]
+pub struct PlumeVerify {
+  range: RangeChip<Fr>,
+  builder: Rc<RefCell<BaseCircuitBuilder<Fr>>>,
+}
+
+#[wasm_bindgen]
+impl PlumeVerify {
+  #[wasm_bindgen(constructor)]
+  pub fn new(circuit: &Halo2Wasm) -> Self {
+    let builder = Rc::clone(&circuit.circuit);
+    let lookup_bits = match builder.borrow_mut().lookup_bits() {
+      Some(x) => x,
+      None => panic!("Lookup bits not found"),
+    };
+    let lookup_manager = builder.borrow_mut().lookup_manager().clone();
+    let range = RangeChip::<Fr>::new(lookup_bits, lookup_manager);
+    PlumeVerify {
+      range,
+      builder: Rc::clone(&circuit.circuit),
+    }
+  }
+
+  pub fn run(&mut self, input: PlumeVerifyInput) {
+    let nullifier = parse_compressed_point(input.nullifier);
+    let s = parse_scalar(input.s);
+    let c = parse_scalar(input.c);
+    let pk = parse_compressed_point(input.public_key);
+    let m = input.message
+      .as_bytes()
+      .iter()
+      .map(|m| Fr::from(*m as u64))
+      .collect::<Vec<_>>();
+
+    let mut builder_borrow = self.builder.borrow_mut();
+    let ctx = builder_borrow.main(0);
+    let range = &self.range;
+
+    let fp_chip = FpChip::<Fr>::new(&range, 88, 3);
+    let fq_chip = FqChip::<Fr>::new(&range, 88, 3);
+    let ecc_chip = EccChip::<Fr, FpChip<Fr>>::new(&fp_chip);
+
+    let mut poseidon_hasher = PoseidonHasher::<Fr, 3, 2>::new(
+      OptimizedPoseidonSpec::new::<8, 57, 0>()
+    );
+    poseidon_hasher.initialize_consts(ctx, range.gate());
+
+    let nullifier = ecc_chip.load_private_unchecked(ctx, (nullifier.x, nullifier.y));
+    let s = fq_chip.load_private(ctx, s);
+    let c = fq_chip.load_private(ctx, c);
+    let pk = ecc_chip.load_private_unchecked(ctx, (pk.x, pk.y));
+    let m = m
+      .iter()
+      .map(|m| ctx.load_witness(*m))
+      .collect::<Vec<_>>();
+
+    let plume_input = PlumeInput::<Fr> {
+      nullifier,
+      s,
+      c,
+      pk,
+      m,
+    };
+
+    verify_plume::<Fr>(ctx, &ecc_chip, &poseidon_hasher, 4, 4, plume_input)
+  }
+}
---
+++ circuits/halo2/wasm/src/utils.rs
@@ -0,0 +1,35 @@
+use halo2_wasm::{
+  halo2_base::utils::{ biguint_to_fe, ScalarField },
+  halo2_proofs::{ arithmetic::CurveAffine, halo2curves::ff::PrimeField },
+  halo2lib::ecc::{ Secp256k1Affine, Secp256k1Fp as Fp, Secp256k1Fq as Fq },
+};
+use num_bigint::BigUint;
+use num_traits::Num;
+
+pub fn parse_compressed_point(compressed_pt: String) -> Secp256k1Affine {
+  let bytes = BigUint::from_str_radix(&compressed_pt[2..], 16)
+    .unwrap()
+    .to_bytes_le();
+
+  let y_is_odd = bytes[bytes.len() - 1] == 3;
+
+  let x = BigUint::from_bytes_le(&bytes[..bytes.len() - 1]);
+
+  let modulus = BigUint::from_str_radix(&Fp::MODULUS.to_string()[2..], 16).unwrap();
+  let y2 = (x.modpow(&BigUint::from(3u64), &modulus) + BigUint::from(7u64)) % &modulus;
+  let mut y = y2.modpow(&((modulus.clone() + BigUint::from(1u64)) / BigUint::from(4u64)), &modulus);
+  if y.bit(0) != y_is_odd {
+    y = modulus - y;
+  }
+
+  Secp256k1Affine::from_xy(biguint_to_fe::<Fp>(&x), biguint_to_fe::<Fp>(&y)).unwrap()
+}
+
+pub fn parse_scalar(s: String) -> Fq {
+  ScalarField::from_bytes_le(
+    BigUint::from_str_radix(&s[2..], 16)
+      .unwrap()
+      .to_bytes_le()
+      .as_slice()
+  )
+}
